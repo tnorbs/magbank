815b7c7f48cfc6e2bca8d47a37a91bca88026cb3
Thais Norberlandi <thaisnorberlandi@gmail.com>
2020-02-05 20:18:30 -0300

    feat(mag-faq): Finished JavaScript implementation

--- src/script.js
+++ src/script.js
@@ -1,10 +1,14 @@
-/* Document ready */ 
+/* Document ready -> implementação do jQuery */ 
 
 $(document).ready(() => {
 
+    // adicionando texto inicial no mag-faq-title
+    $('.mag-faq-title').text('Cartão de crédito e débito');
 
     $('.mag-faq .mag-icon-and-text i').on('click', function () {
+        // pegando o data-content somente do <i> que foi clicado
         const content = $(this).data('content');
-        alert(content);
+        // pegando o h3 e colocando o texto do data-content como conteúdo quando o íncone for clicado
+        $('.mag-faq-title').text(content);
     });
 });
