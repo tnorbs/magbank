4ac7685a0c7713a340992816d128851affc825b2
Thais Norberlandi <thaisnorberlandi@gmail.com>
2020-02-05 21:43:52 -0300

    feat(faq): Finished FAQ's  design and JavaScript implementation

--- src/script.js
+++ src/script.js
@@ -5,10 +5,20 @@ $(document).ready(() => {
     // adicionando texto inicial no mag-faq-title
     $('.mag-faq-title').text('Cartão de crédito e débito');
 
-    $('.mag-faq .mag-icon-and-text i').on('click', function () {
+    // adicionando a cor branca para o primeiro ícone da seção do mag-faq
+    $('.mag-faq .mag-icon-and-text').first().css('color', '#FFF');
+
+    $('.mag-faq .mag-icon-and-text').on('click', function () {
         // pegando o data-content somente do <i> que foi clicado
-        const content = $(this).data('content');
-        // pegando o h3 e colocando o texto do data-content como conteúdo quando o íncone for clicado
+        const content = $(this).find('i').data('content');
+
+        // pegando o h3 e colocando o texto do data-content como conteúdo quando o ícone for clicado
         $('.mag-faq-title').text(content);
+
+        // deixando todos os mag-icon-and-text cinza
+        $('.mag-faq .mag-icon-and-text').css('color', '#a8a8a8');
+
+        // deixando a cor do texto em branco
+        $(this).css('color', '#fff');
     });
 });
